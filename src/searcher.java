f0777655f8de3612563b9ff3f3854a81ffb398ef
yunjeongiya <yunjeongiragu@gmail.com>
2021-04-08 01:40:59 +0900

    feat: add Searcher for calculating similarity (week 5)

--- src/searcher.java
+++ src/searcher.java
@@ -1,4 +1,3 @@
-import jdk.internal.access.JavaNetHttpCookieAccess;
 import org.jsoup.Jsoup;
 import org.snu.ids.kkma.index.Keyword;
 import org.snu.ids.kkma.index.KeywordExtractor;
@@ -30,7 +29,6 @@ public class searcher {
         return (HashMap) object;
     }
 
-    //문서 아이디, 쿼리와의 유사도로 구성된 pair 들의 list 출력
     public PriorityQueue<indexer.Pair> CalcSim(String query, HashMap<String, List<indexer.Pair>> indexPOST) throws IOException, ClassNotFoundException {
 
         KeywordExtractor ke = new KeywordExtractor();
@@ -38,40 +36,40 @@ public class searcher {
         HashMap<String, Double> querymap = new HashMap<>();
         for (int i=0; i<kl.size(); i++){
             Keyword kwrd = kl.get(i);
-            querymap.put(kwrd.getString(), 1.0)
+            querymap.put(kwrd.getString(), 1.0);
         }
         PriorityQueue<indexer.Pair> pq = new PriorityQueue();
-
-        for (String key : indexPOST.keySet()) {
+        HashMap<String, Double> map  =  new HashMap<>();
+        for (String key : querymap.keySet()) {
+            if (!indexPOST.containsKey(key))
+                continue;
+            double queryWeight = querymap.get(key);
             List<indexer.Pair> pairList = indexPOST.get(key);
-            double weight = 0;
             for (indexer.Pair pair : pairList) {
                 String postId = pair.postId;
                 double val = pair.val;
-                if (!querymap.containsKey(postId))
-                    continue;
-                weight += val * querymap.get(postId);
+                if (!map.containsKey(postId))
+                    map.put(postId, 0.0);
+                map.put(postId, map.get(postId) + val * queryWeight);
             }
-            indexer.Pair res = new indexer.Pair(key, weight);
-            pq.add(res);
         }
+        for (String key : map.keySet())
+            pq.add(new indexer.Pair(key, map.get(key)));
         return pq;
     }
 
-    //pair들의 list를 받아서 상위 3개 출력.
-    //POSTPath를 받아와서 collectionPath 구한 뒤 title parse
     public void printResult(PriorityQueue<indexer.Pair> similarity, String POSTPath) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-        Document doc = docBuilder.parse(new File(POSTPath.replace("index.post", "collection.xml"));
-        String title = toString(doc.getElementsByTagName("title"));
+        Document doc = docBuilder.parse(new File(POSTPath.replace("index.post", "collection.xml")));
 
         for(int i=0; i<3 && !similarity.isEmpty(); i++){
 
-            indexer.Pair pair = similarity.pop();
+            String title = new String(String.valueOf(doc.getElementsByTagName("title")));
 
-            System.out.println("title: " +  bodyList(pair.postId) + ", result :"  + String.format("%.2f", pair.val));
+            indexer.Pair pair = similarity.poll();
+
+            System.out.println("title: " + (pair.postId)/* title */ + ", result :"  + String.format("%.2f", pair.val));
         }
     }
-
 }
