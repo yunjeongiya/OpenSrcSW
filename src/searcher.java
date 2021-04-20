89cd8a7ab9c25dbe58f7b3ed0f9282cb8deb5f62
yunjeongiya <yunjeongiragu@gmail.com>
2021-04-20 12:04:28 +0900

    add searcher to master branch

---
+++ src/searcher.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class searcher {
+    public PriorityQueue<indexer.Pair> CalcSim(String query, HashMap<String, List<indexer.Pair>> indexPOST){
+        PriorityQueue cosSim = new PriorityQueue();
+
+        return cosSim;
+    }
+}
